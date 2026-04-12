b55432b21a96a9593547cfa6215a87779b374f01
wilanymelo <wilany2014moura@gmail.com>
2026-04-11 22:48:49 -0300

    Implementação de CRUD de livros com rotas GET,POST, PUT e DELETE

--- Server.js
+++ Server.js
@@ -4,30 +4,42 @@ import fs from 'fs';
 
 
 //aprendendo a usar o express, criei um servidor básico.
-const app = express();  
-const PORT = 3000;
-
+const app = express();
 app.use(express.json());
+
+const livros = JSON.parse(fs.readFileSync('livros.json', 'utf-8'));
+const PORT = 3000;
 
 
 app.use(express.static('Front/src'));
 
+// Rota de teste para verificar se o servidor está funcionando
 app.get('/users', (req, res) => {
 
     res.send('oK, DEU CERTO');
 
 });
 
+// Rotas para CRUD de livros
+
+// Rota para exibir a lista de livros
+app.get("/livros", (req, res) => {
+ 
+  res.status(200).json(livros);
+  
+});
+
+// Rota para buscar um livro por id
 app.get("/livros/:id", (req, res) => {
   const index = buscaLivro(req.params.id);
   if (index === -1) {
-    res.status(404).send("Livro não encontrado");
+    res.status(404).json("Livro não encontrado");
   } else {
     res.status(200).json(livros[index]);
   }
 });
 
-
+// Rota para criar um novo livro
 app.post('/livros', (req, res) => {
   const { title, author } = req.body;
 
@@ -40,12 +52,14 @@ app.post('/livros', (req, res) => {
 
   // simulando ID automático
   const novoLivro = {
-    id: Date.now(),
-    title,
-    author
+    id: livros.length > 0 
+    ? livros[livros.length - 1].id + 1 
+    : 1,
+   title,
+   author
   };
 
-  books.push(novoLivro);
+  livros.push(novoLivro);
 
   res.status(201).json({
     mensagem: "Livro adicionado com sucesso",
@@ -53,21 +67,23 @@ app.post('/livros', (req, res) => {
   });
 });
 
+// Rota para atualizar um livro existente
 app.put("/livros/:id", (req, res) => {
   const index = buscaLivro(req.params.id);
   if (index === -1) {
-    res.status(404).send("Livro não encontrado para atualizar");
+    return res.status(404).json({erro:"Livro não encontrado para atualizar"});
   } else {
-    livros[index].titulo = req.body.titulo;
-    livros[index].autor = req.body.autor;
+    livros[index].title = req.body.title;
+    livros[index].author = req.body.author;
     res.status(200).json(livros[index]);
   }
 });
 
+// Rota para deletar um livro
 app.delete("/livros/:id", (req, res) => {
   const index = buscaLivro(req.params.id);
   if (index === -1) {
-    res.status(404).send("Livro não encontrado para remover");
+    res.status(404).json("Livro não encontrado para remover");
   } else {
     livros.splice(index, 1);
     res.status(200).send("Livro removido com sucesso");
